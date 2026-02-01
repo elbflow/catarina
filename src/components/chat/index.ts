009262ce6a330edea8f9588ee328353a325abd3d
Felix Larsen <16463534+felix-larsen@users.noreply.github.com>
2026-02-01 11:24:35 +0100

    feat(chat): add index exports for chat components

---
+++ src/components/chat/index.ts
@@ -0,0 +1,4 @@
+export { ChatProvider, useChatPanel } from './ChatProvider'
+export { ChatPane } from './ChatPane'
+export { ChatMessage } from './ChatMessage'
+export { ChatToggle } from './ChatToggle'
